d104e2a9123001bb232af2c6d5be73a132fef1a5
ONLY1BTAYY <ogxclusiv@gmail.com>
2026-04-07 08:32:23 -0500

    Auto-run g2w after install + preuninstall cleanup script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,1 +1,6 @@
+const path = require('path');
+const { spawnSync } = require('child_process');
+
 require('../lib/install').run();
+
+spawnSync(process.execPath, [path.join(__dirname, '../bin/g2w.js')], { stdio: 'inherit' });
---
+++ scripts/preuninstall.js
@@ -0,0 +1,1 @@
+require('../lib/install').uninstall();
